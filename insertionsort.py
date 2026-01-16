ecb19b45795e0c3beb13b8b66b394046aa313315
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2026-01-16 10:37:12 +0530

    Add files via upload

---
+++ insertionsort.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    n = len(arr)
+    
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        arr[j + 1] = key
+
+    return arr
+
+
+# Example
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(insertion_sort(arr))
